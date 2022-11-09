18930b79e533f76d22ed4b1294ed7c064c75d1a4
kamranuz <i@kamran.uz>
2022-11-09 23:06:43 +0300

    metrics V4.2

--- backend.py
+++ backend.py
@@ -11,7 +11,7 @@ def get_metrics(df):
     metrics_relation = df.groupby(['url_start','url_end']).agg({'diff':['mean','max','min','sum','count'],'reload':['mean'], 'process_flag_num':['mean']})
     metrics_relation.columns = [ '_'.join(col) for col in metrics_relation.columns]
     metrics_relation = metrics_relation.rename(columns={'diff_count':'qty_rel_all','process_flag_num_mean':'process_order_mean','reload_mean':'is_reload'})
-    metrics_relation
+    metrics_relation = metrics_relation.reset_index()
 
     qty_all = df.groupby('url_start').count().rename(columns={'clientcode':'qty_all'})[['qty_all']]
     qty_unique_user = df[['clientcode','url_start']].drop_duplicates().groupby('url_start').count().rename(columns={'clientcode':'qty_unique_user'})
@@ -24,6 +24,6 @@ def get_metrics(df):
     metrics_node = pd.merge(metrics_node,qty_in,left_index=True, right_index=True, how = 'left')
     metrics_node = pd.merge(metrics_node,qty_out,left_index=True, right_index=True, how = 'left')
     metrics_node['qty_in_out_ratio'] = metrics_node.qty_in/metrics_node.qty_out
-    metrics_node
+    metrics_node = metrics_node.reset_index()
 
     return metrics_node, metrics_relation
